4e9f78c87e01da1566888898cf9160b0820bb86e
huanyinyueye <lightfugitive@gmail.com>
2021-12-10 16:27:50 +0800

    delete useless code

--- index.js
+++ index.js
@@ -4,38 +4,31 @@ const _PORT = 3000
 const { getBooks, selectBook, createBook, deleteBook, updateBook } = require('./contollers/book')
 const { getUsers, selectUser, createUser, deleteUser, updateUser } = require('./contollers/user')
 app.use(express.urlencoded({ extended: true })) // Note: add this to trigger post action
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 // frontend
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('pages/login');
 });
 
-
-app.get('/main', function(req, res) {
+app.get('/main', function (req, res) {
   res.render('pages/index');
 });
 
-app.get('/book', function(req, res) {
+app.get('/book', function (req, res) {
   res.render('pages/book');
 });
 
-app.get('/user', function(req, res) {
+app.get('/user', function (req, res) {
   res.render('pages/user');
 });
-
-app.get('/users', async (req, res) => {
-  const users = await Users.findAll()
-
-})
 
 // backend
 const booksRouter = express.Router()
 
 booksRouter.route('/book').get(getBooks).post(createBook)
 booksRouter.route('/book/delete').post(deleteBook)
-booksRouter.route('/book/update').post(updateBook)    
+booksRouter.route('/book/update').post(updateBook)
 booksRouter.get('/book/:bookId', selectBook)
 
 app.use('/book', booksRouter)
@@ -47,11 +40,11 @@ usersRouter.route('/user/delete').post(deleteUser)
 usersRouter.route('/user/update').post(updateUser)
 usersRouter.get('/user/:userId', selectUser)
 
-app.use('/user',usersRouter)
+app.use('/user', usersRouter)
 
 const mainRouter = express.Router()
 
-app.use('/main',mainRouter)
+app.use('/main', mainRouter)
 
 app.listen(_PORT, () => {
   console.log(`Express server is running at http://localhost:${_PORT}`)
